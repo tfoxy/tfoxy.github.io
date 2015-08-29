fe9ec8934d2768dd3e76d5a20568889456e8f41e
Tomás Fox <tomas.c.fox@gmail.com>
2015-08-28 22:48:46 -0300

    Upload angular-katex

---
+++ b/angular-katex/angular-katex.js
@@ -0,0 +1,119 @@
+/*!
+ * angular-katex v0.7.0
+ * https://github.com/tfoxy/angular-katex
+ *
+ * Copyright 2015 Tomás Fox
+ * Released under the MIT license
+ */
+
+(function() {
+  'use strict';
+
+  angular.module('katex', [])
+      .constant('katex', katex)
+      .factory('katexConfig', katexConfigFactory)
+      .directive('katex', katexDirective)
+      .directive('katexBind', katexBindDirective)
+      .directive('katexHtml', katexHtmlDirective);
+
+
+  katexConfigFactory.$inject = ['katex', '$document'];
+
+  function katexConfigFactory(katex, $document) {
+    var service = {
+      defaultOptions: {},
+      errorHandler: function(err, expr, element) {
+        var span = $document[0].createElement('span');
+        span.className = 'katex-error';
+        span.textContent = err;
+        element.children().remove();
+        element.append(span);
+      },
+      render: function(element, expr, elementOptions, errorHandler) {
+        try {
+          var options = elementOptions || service.defaultOptions;
+          katex.render(expr || '', element[0], options);
+        } catch (err) {
+          errorHandler(err, expr, element);
+        }
+      }
+    };
+
+    return service;
+  }
+
+
+  katexDirective.$inject = ['katexConfig', '$rootScope'];
+
+  function katexDirective(katexConfig, $rootScope) {
+    return {
+      restrict: 'AE',
+      compile: function(element, attrs) {
+        var expr = attrs.katex || element.text();
+        var options = getOptions($rootScope, attrs);
+        var errorHandler = getErrorHandler($rootScope, attrs, katexConfig);
+        katexConfig.render(element, expr, options, errorHandler);
+      }
+    };
+  }
+
+
+  katexBindDirective.$inject = ['katexConfig'];
+
+  function katexBindDirective(katexConfig) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        var model = attrs.katexBind;
+        var errorHandler = getErrorHandler(scope, attrs, katexConfig);
+
+        scope.$watch(model, function(expr) {
+          var options = getOptions(scope, attrs);
+          katexConfig.render(element, expr, options, errorHandler);
+        });
+      }
+    };
+  }
+
+
+  katexHtmlDirective.$inject = ['katexConfig', '$rootScope'];
+
+  function katexHtmlDirective(katexConfig, $rootScope) {
+    return {
+      restrict: 'AE',
+      compile: function(element, attrs) {
+        var exprElement = attrs.katexHtml ?
+            angular.element('<div>' + attrs.katexHtml + '</div>') :
+            element;
+        var options = getOptions($rootScope, attrs);
+        var errorHandler = getErrorHandler($rootScope, attrs, katexConfig);
+        katexConfig.render(element, exprElement.html(), options, errorHandler);
+      }
+    };
+  }
+
+
+  function getOptions(scope, attrs) {
+    var katexOptions = attrs.katexOptions;
+    return katexOptions && scope.$eval(katexOptions);
+  }
+
+
+  function getErrorHandler(scope, attrs, katexConfig) {
+    var katexOnError = attrs.katexOnError;
+
+    if (katexOnError) {
+      return function katexOnErrorFn(err, expr, element) {
+        scope.$eval(katexOnError, {
+          $err: err,
+          $expr: expr,
+          $setText: function(text) {
+            element.text(text);
+          }
+        });
+      };
+    }
+
+    return katexConfig.errorHandler;
+  }
+})();
